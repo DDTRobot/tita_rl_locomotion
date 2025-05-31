19d784884a8c2eeedaeba08bf081e6b86c6e5bca
yankai <1924286463@qq.com>
2025-05-31 23:46:55 +0800

    Update FSMState_RL.cpp
    Signed-off-by: yankai <1924286463@qq.com>

--- tita_controllers/tita_controller/src/fsm/FSMState_RL.cpp
+++ tita_controllers/tita_controller/src/fsm/FSMState_RL.cpp
@@ -126,11 +126,11 @@ void FSMState_RL::run()
   {
     if(i % 4 == 3)
     {
-      _data->low_cmd->tau_cmd[i] = 12 * desired_pos[i] + 1.5 * (0 - _data->low_state->dq[i]);
+      _data->low_cmd->tau_cmd[i] = _data->params->rl_joint_kp[i] * desired_pos[i] +  _data->params->rl_joint_kd[i]  * (0 - _data->low_state->dq[i]);
     }
     else
     {
-      _data->low_cmd->tau_cmd[i] = 60 * (desired_pos[i] - _data->low_state->q[i]) + 2.0 * (0 - _data->low_state->dq[i]);
+      _data->low_cmd->tau_cmd[i] =  _data->params->rl_joint_kp[i]  * (desired_pos[i] - _data->low_state->q[i]) + _data->params->rl_joint_kd[i] * (0 - _data->low_state->dq[i]);
     }
   }
 
